d84a096c4138647a651870f964d3ceec5f096895
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-05-29 17:02:11 +0600

    user for each post api integration done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,30 +2,54 @@ import  { useState, useEffect } from "react";
 import axios from "axios";
 import Post from "./Post/Post";
 
-
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchPostsAndUsers = async () => {
       try {
-        const response = await axios.get(
+        // Here i am Fetching posts Data
+        const postsResponse = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/posts`
         );
-        setPosts(response.data);
+        const fetchedPosts = postsResponse.data;
+
+        // Here i am Fetching users Data
+        const usersResponse = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/users`
+        );
+        const fetchedUsers = usersResponse.data;
+
+        // Here i am Matching posts with users based on user ID
+        const postsWithUsername = fetchedPosts.map((post) => {
+          const user = fetchedUsers.find((user) => user.id === post.userId);
+          return {
+            ...post,
+            username: user ? user.username : "No Name",
+          };
+        });
+
+        setPosts(postsWithUsername);
+        setUsers(fetchedUsers);
       } catch (error) {
-        console.error("Error fetching the posts:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
-    fetchPosts();
+    fetchPostsAndUsers();
   }, []);
 
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
-          <Post key={post.id} title={post.title} body={post.body} />
+          <Post
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            username={post.username}
+          />
         ))}
       </div>
     </div>
--- src/components/Home/Post/Post.jsx
+++ src/components/Home/Post/Post.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
-const Post = ({ title, body }) => {
+const Post = ({ title, body, username }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700">{body}</p>
+      <p className="text-red-500">{username}</p>
     </div>
   );
 };
 Post.propTypes = {
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
 };
 export default Post;
