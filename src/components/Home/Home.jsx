595fd275228418874e4220e1ddce07e9d744d153
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2024-05-30 10:30:49 +0600

    comment api implementation and post show comment and less show funcationality done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,37 +1,45 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Post from "./Post/Post";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
-  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchPostsAndUsers = async () => {
       try {
-        // Here i am Fetching posts Data
+        // Here i am Fetching posts data
         const postsResponse = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/posts`
         );
         const fetchedPosts = postsResponse.data;
 
-        // Here i am Fetching users Data
+        // Here i am Fetching users data
         const usersResponse = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/users`
         );
         const fetchedUsers = usersResponse.data;
 
-        // Here i am Matching posts with users based on user ID
-        const postsWithUsername = fetchedPosts.map((post) => {
+        //Here i am Fetching comments data
+        const postCommentResponse = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/comments`
+        );
+        const fetchedComments = postCommentResponse.data;
+
+        // Here i am Matching posts with users and comments
+        const postsWithDetails = fetchedPosts.map((post) => {
           const user = fetchedUsers.find((user) => user.id === post.userId);
+          const postComments = fetchedComments.filter(
+            (comment) => comment.postId === post.id
+          );
           return {
             ...post,
             username: user ? user.username : "No Name",
+            comments: postComments,
           };
         });
 
-        setPosts(postsWithUsername);
-        setUsers(fetchedUsers);
+        setPosts(postsWithDetails);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -39,7 +47,6 @@ const Home = () => {
 
     fetchPostsAndUsers();
   }, []);
-
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
@@ -49,6 +56,7 @@ const Home = () => {
             title={post.title}
             body={post.body}
             username={post.username}
+            comments={post.comments}
           />
         ))}
       </div>
--- src/components/Home/Post/Post.jsx
+++ src/components/Home/Post/Post.jsx
@@ -1,16 +1,62 @@
+import  { useState } from "react";
 import PropTypes from "prop-types";
-const Post = ({ title, body, username }) => {
+
+const Post = ({ title, body, username, comments }) => {
+  const [showComments, setShowComments] = useState(false);
+
+  const toggleComments = () => {
+    setShowComments(!showComments);
+  };
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg">
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      <p className="text-gray-700">{body}</p>
-      <p className="text-red-500">{username}</p>
+      <h2 className="text-xl font-bold mb-2 text-justify capitalize">
+        {title}
+      </h2>
+      <p className="text-gray-700 text-justify capitalize">{body}</p>
+      <p className="text-gray-500 mt-4">
+        Posted by: <b className="text-black">{username}</b>
+      </p>
+      <a
+        className="mt-6  text-black underline cursor-pointer py-2 px-4 "
+        onClick={toggleComments}
+      >
+        {showComments ? "Show Less" : "Show More"}
+      </a>
+      {showComments && (
+        <div className="mt-4">
+          <h3 className="text-xl font-bold">Comments:</h3>
+          {comments.length > 0 ? (
+            comments.map((comment) => (
+              <div key={comment.id} className="mt-2">
+                <p className="font-semibold text-justify capitalize">
+                  {comment.name}
+                </p>
+                <p className="text-gray-600 text-justify capitalize mt-3">
+                  {comment.body}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>No comments</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
+
 Post.propTypes = {
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
+
 export default Post;
